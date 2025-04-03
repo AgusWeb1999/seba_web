1a82bbeb746a4dae2139e9e887cfa65e1d0804ef
maximiliano836 <172645263+maxiTiendamia@users.noreply.github.com>
2025-04-03 14:00:46 -0300

    iniciando logos nuevos y responsive del nav

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/NavComponent.css'; // Importa los estilos del navbar
-
+import '../css/NavComponent.css'; 
+import logo from '../images/logo_principal.png';
+import logo_secundario from '../images/logo_secundario.png';
 const NavComponent = () => {
   return (
     <nav className="navbar">
-      <div className="logo">
-        <Link to="/">Steelframing</Link>
-      </div>
+      <Link to="/" className="logo">
+        <img src={logo} alt="Logo principal" />
+      </Link>      
+      <Link to="/" className="logo_secundario">
+        <img src={logo_secundario} alt="Logo principal" />
+      </Link>   
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About Us</Link></li>
