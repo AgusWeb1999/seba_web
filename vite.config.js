50b726bc2518a271fdbd77da05d937e413fdd388
maximiliano836 <maxi10.02.00.g@gmail.com>
2025-04-07 19:41:24 -0300

    prueba

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/seba_web/', // 👈 este valor debe coincidir con el nombre del repo
+  base: '/seba_web', 
 });
