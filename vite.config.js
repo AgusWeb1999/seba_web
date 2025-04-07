b4fc89e1121f3ccaf429dfa2108bbc647c744bcd
Agustin Mazzini <agustin.mazzini@tiendamia.com>
2025-04-07 11:57:29 -0300

    volvemos a la version anterior de vite archivo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/seba_web/', // 👈 este valor debe coincidir con el nombre del repo
-
+  server: {
+    // Redirige todas las rutas al archivo index.html
+    hmr: true,
+    historyApiFallback: true,
+  },
 });
