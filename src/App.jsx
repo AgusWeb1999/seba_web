703f90997d47b3f55b923d6cd62b72a6817e0014
Agustin Mazzini <agustin.mazzini@tiendamia.com>
2025-04-10 14:00:30 -0300

    fix redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from "react-router-dom"; 
 import CarouselComponent from "./components/CarouselComponent";
 import FooterComponent from "./components/FooterComponent";
